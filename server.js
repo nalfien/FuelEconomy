6e3200ad40e36bfecde3ba7a8510a73fbe728ae0
Eric Remington <nalfien@gmail.com>
2018-05-19 11:50:12 -0400

    Heroku update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fuel-economy'));
+app.use(express.static('/dist/fuel-economy'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fuel-economy/index.html'));
+res.sendFile(path.join('/dist/fuel-economy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
